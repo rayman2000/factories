e0c1d833cfc2ab7e42ef84ed392542b6fb01c74f
Nicolas Klose <nicolas.klose@inf.ethz.ch>
2024-02-08 14:00:56 +0100

    fixed broken stuff

--- factorio.py
+++ factorio.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from functools import lru_cache
 from math import ceil
-import re
 from typing import Optional
 import luadata  # type: ignore
 
@@ -28,12 +27,12 @@ class Recipe:
 
     def to_string(self, num: float = 1) -> str:
         input_string = ", ".join(
-            [f"{amount * num} {inp}" for inp, amount in self.inputs.items()]
+            [f"{round(amount, 2)} {inp}" for inp, amount in self.in_per_second(num).items()]
         )
         output_string = ", ".join(
-            [f"{amount * num} {out}" for out, amount in self.outputs.items()]
+            [f"{round(amount, 2)} {out}" for out, amount in self.out_per_second(num).items()]
         )
-        return f"{round(num, 2)} {self.category} creating {output_string} from {input_string} every {self.sec} seconds"
+        return f"{round(num, 2)} {self.category} creating {output_string} from {input_string} per second"
 
 
 raw = luadata.read("recipes/factorio.lua")
@@ -41,14 +40,17 @@ raw = luadata.read("recipes/factorio.lua")
 recipes: list[Recipe] = []
 
 def parse_ingredient(ing):
-
     if isinstance(ing, dict):
         return (ing["name"], ing["amount"])
     else:
         return (ing[0], ing[1])
+    
+def get_crafting_speed(category: str) -> float:
+    return {"assembling": 0.75, "crafting-with-fluid": 0.75, "smelting": 2}.get(category, 1)
 
 for rec in raw:
-    sec = rec.get("energy_required", 0.5)
+    cat = rec.get("category", "assembling")
+    sec = rec.get("energy_required", 0.5) * get_crafting_speed(cat)
     details = rec.get("normal", rec)
     inputs = {parse_ingredient(ing)[0] : parse_ingredient(ing)[1] for ing in details.get("ingredients")}
     if details.get("result") is not None:
@@ -56,9 +58,7 @@ for rec in raw:
     else:
         outputs = {parse_ingredient(ing)[0] : parse_ingredient(ing)[1] for ing in details.get("results")}
 
-    recipe = Recipe(
-        rec["name"], inputs, outputs, sec, rec.get("category", "assembling")
-    )
+    recipe = Recipe(rec["name"], inputs, outputs, sec, cat)
     recipes.append(recipe)
 
 items_raw = {out for rec in recipes for out in rec.outputs}
@@ -77,10 +77,10 @@ def get_default_recipe(item: str) -> Optional[Recipe]:
     return recs[0]
 
 @lru_cache
-def get_recipe(name: str) -> Optional[Recipe]:
+def get_recipe(name: str) -> Recipe:
     recs = [rec for rec in recipes if rec.name == name]
-    if len(recs) == 0:
-        return None
+    #if len(recs) == 0:
+    #    return None
     assert len(recs) == 1
     return recs[0]
 
@@ -123,7 +123,7 @@ def add_recipe(
         required_items[out] -= amount
 
 
-def get_factory(output: dict[str, float], provided: list[int], excess=True):
+def get_factory(output: dict[str, float], provided: list[str], excess=True):
     required_items: dict[str, float] = {item: 0 for item in items}
     for item, amount in output.items():
         rec = get_default_recipe(item)
@@ -158,9 +158,9 @@ def get_factory(output: dict[str, float], provided: list[int], excess=True):
 
     for item, amount in required_items.items():
         if amount > 0:
-            print(f"Input {round(amount * 60)} {item} ")
+            print(f"Input {round(amount)} {item} ")
         if amount < 0:
-            print(f"Excess {-round(amount * 60)} {item}")
+            print(f"Excess {-round(amount)} {item}")
 
 
-get_factory({"automation-science-pack": 1}, [])
+get_factory({"utility-science-pack": 0.25}, ["iron-plate", "copper-plate", "stone-brick", "plastic-bar", "lubricant", "sulfur"], False)
