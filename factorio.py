717dc9bd7bdb3013140ad67d3cb01d6f970b4faa
Nicolas Klose <nicolas.rayklose@gmail.com>
2024-02-09 02:04:46 +0100

    more fixes

--- factorio.py
+++ factorio.py
@@ -27,10 +27,16 @@ class Recipe:
 
     def to_string(self, num: float = 1) -> str:
         input_string = ", ".join(
-            [f"{round(amount, 2)} {inp}" for inp, amount in self.in_per_second(num).items()]
+            [
+                f"{round(amount, 2)} {inp}"
+                for inp, amount in self.in_per_second(num).items()
+            ]
         )
         output_string = ", ".join(
-            [f"{round(amount, 2)} {out}" for out, amount in self.out_per_second(num).items()]
+            [
+                f"{round(amount, 2)} {out}"
+                for out, amount in self.out_per_second(num).items()
+            ]
         )
         return f"{round(num, 2)} {self.category} creating {output_string} from {input_string} per second"
 
@@ -39,24 +45,39 @@ raw = luadata.read("recipes/factorio.lua")
 
 recipes: list[Recipe] = []
 
+
 def parse_ingredient(ing):
     if isinstance(ing, dict):
         return (ing["name"], ing["amount"])
     else:
         return (ing[0], ing[1])
-    
+
+
 def get_crafting_speed(category: str) -> float:
-    return {"assembling": 0.75, "crafting-with-fluid": 0.75, "smelting": 2}.get(category, 1)
+    return {
+        "assembling": 0.75,
+        "crafting-with-fluid": 0.75,
+        "crafting": 0.75,
+        "advanced-crafting": 0.75,
+        "smelting": 2,
+    }.get(category, 1)
+
 
 for rec in raw:
     cat = rec.get("category", "assembling")
-    sec = rec.get("energy_required", 0.5) * get_crafting_speed(cat)
     details = rec.get("normal", rec)
-    inputs = {parse_ingredient(ing)[0] : parse_ingredient(ing)[1] for ing in details.get("ingredients")}
+    sec = details.get("energy_required", 0.5) / get_crafting_speed(cat)
+    inputs = {
+        parse_ingredient(ing)[0]: parse_ingredient(ing)[1]
+        for ing in details.get("ingredients")
+    }
     if details.get("result") is not None:
         outputs = {details["result"]: details.get("result_count", 1)}
     else:
-        outputs = {parse_ingredient(ing)[0] : parse_ingredient(ing)[1] for ing in details.get("results")}
+        outputs = {
+            parse_ingredient(ing)[0]: parse_ingredient(ing)[1]
+            for ing in details.get("results")
+        }
 
     recipe = Recipe(rec["name"], inputs, outputs, sec, cat)
     recipes.append(recipe)
@@ -76,10 +97,11 @@ def get_default_recipe(item: str) -> Optional[Recipe]:
 
     return recs[0]
 
+
 @lru_cache
 def get_recipe(name: str) -> Recipe:
     recs = [rec for rec in recipes if rec.name == name]
-    #if len(recs) == 0:
+    # if len(recs) == 0:
     #    return None
     assert len(recs) == 1
     return recs[0]
@@ -128,7 +150,7 @@ def get_factory(output: dict[str, float], provided: list[str] = [], excess=True)
     for item, amount in output.items():
         rec = get_default_recipe(item)
         assert rec is not None
-        required_items[item] += amount * rec.outputs[item]
+        required_items[item] += amount
 
     required_recipes: dict[str, float] = {}
 
@@ -149,7 +171,7 @@ def get_factory(output: dict[str, float], provided: list[str] = [], excess=True)
                     number = ceil(number)
 
                 # Recursive stuff breaks here
-                    # Crack heavy and light oil if it not required
+                # Crack heavy and light oil if it not required
 
                 add_recipe(rec, number, required_recipes, required_items)
 
@@ -164,5 +186,32 @@ def get_factory(output: dict[str, float], provided: list[str] = [], excess=True)
             print(f"Excess {-round(amount)} {item}")
 
 
-#get_factory({"utility-science-pack": 0.25}, ["iron-plate", "copper-plate", "stone-brick", "plastic-bar", "lubricant", "sulfur"], False)
-get_factory({"sulfur": 6})
+# get_factory({"automation-science-pack": 1}, ["iron-plate", "copper-plate"])
+
+
+get_factory(
+    {"production-science-pack": 0.3},
+    [
+        "iron-plate",
+        "copper-plate",
+        "sulfur",
+        "plastic-bar",
+        "advanced-circuit",
+        "electric-furnace",
+    ],
+    False,
+)
+# get_factory({"engine-unit": 0.1}, ["iron-plate", "copper-plate"])
+# get_factory({"military-science-pack": 0.1}, ["iron-plate", "copper-plate"])
+# get_factory(
+#    {"utility-science-pack": 0.22},
+#    [
+#        "iron-plate",
+#        "copper-plate",
+#        "stone-brick",
+#        "plastic-bar",
+#        "lubricant",
+#        "sulfur",
+#    ],
+#    False,
+# )
